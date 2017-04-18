079d9674651f96f559a2d3ff64114f88a0493521
Andrey Pavlov <couchemar@yandex.ru>
2017-04-18 15:16:40 +0300

    Simplify

--- pkgs/servers/sql/agensgraph/default.nix
+++ pkgs/servers/sql/agensgraph/default.nix
@@ -1,82 +1,35 @@
 { lib, stdenv, glibc, fetchurl, zlib, readline, libossp_uuid, openssl, makeWrapper, perl, bison, flex }:
 
-let
+ 
+stdenv.mkDerivation rec {
+  name = "agensgraph-${version}";
+  version = "1.1.0";
 
-  common = { version, pgVersion, sha256, psqlSchema } @ args:
-   let atLeast = lib.versionAtLeast pgVersion; in stdenv.mkDerivation (rec {
-    name = "agensgraph-${version}";
+  src = fetchurl {
+    url = "https://github.com/bitnine-oss/agensgraph/archive/v${version}.tar.gz";
+    sha256 = "0qbmzmkl36wv5d66k7s0p0xll4ai1ax3pczrfgxbmfk9i2j1gjzw";
+  };
 
-    src = fetchurl {
-      url = "https://github.com/bitnine-oss/agensgraph/archive/v${version}.tar.gz";
-      inherit sha256;
-    };
+  # outputs = [ "out" "lib" ];
+  # setOutputFlags = false; # $out retains configureFlags :-/
 
-    outputs = [ "out" "lib" "doc" ];
-    setOutputFlags = false; # $out retains configureFlags :-/
+  buildInputs =
+    [ readline zlib perl bison flex ];
 
-    buildInputs =
-      [ zlib readline openssl makeWrapper perl bison flex ]
-      ++ lib.optionals (!stdenv.isDarwin) [ libossp_uuid ];
+  enableParallelBuilding = true;
 
-    enableParallelBuilding = true;
+#  makeFlags = [ "world" ];
 
-#    makeFlags = [ "world" ];
+  # configureFlags = [
+  #   "--with-openssl"
+  #   "--sysconfdir=/etc"
+  #   "--libdir=$(lib)/lib"
+  #   "--with-ossp-uuid"
+  # ];
 
-    configureFlags = [
-      "--with-openssl"
-      "--sysconfdir=/etc"
-      "--libdir=$(lib)/lib"
-    ]
-      ++ lib.optional (stdenv.isDarwin)  "--with-uuid=e2fs"
-      ++ lib.optional (!stdenv.isDarwin) "--with-ossp-uuid";
+  installTargets = [ "install" ];
 
-    patches =
-      [ (if atLeast "9.4" then ./disable-resolve_symlinks-94.patch else ./disable-resolve_symlinks.patch)
-        (if atLeast "9.6" then ./less-is-more-96.patch             else ./less-is-more.patch)
-        (if atLeast "9.6" then ./hardcode-pgxs-path-96.patch       else ./hardcode-pgxs-path.patch)
-        ./specify_pkglibdir_at_runtime.patch
-      ];
-
-#    installTargets = [ "install-world" ];
-
-    LC_ALL = "C";
-
-    postConfigure =
-      let path = if atLeast "9.6" then "src/common/config_info.c" else "src/bin/pg_config/pg_config.c"; in
-        ''
-          # Hardcode the path to pgxs so pg_config returns the path in $out
-          substituteInPlace "${path}" --replace HARDCODED_PGXS_PATH $out/lib
-        '';
-
-    postInstall =
-      ''
-        moveToOutput "lib/pgxs" "$out" # looks strange, but not deleting it
-        moveToOutput "lib/*.a" "$out"
-        moveToOutput "lib/libecpg*" "$out"
-
-        # Prevent a retained dependency on gcc-wrapper.
-        substituteInPlace "$out/lib/pgxs/src/Makefile.global" --replace ${stdenv.cc}/bin/ld ld
-
-        # Remove static libraries in case dynamic are available.
-        for i in $out/lib/*.a; do
-          name="$(basename "$i")"
-          if [ -e "$lib/lib/''${name%.a}.so" ] || [ -e "''${i%.a}.so" ]; then
-            rm "$i"
-          fi
-        done
-      '';
-
-    postFixup = lib.optionalString (!stdenv.isDarwin)
-      ''
-        # initdb needs access to "locale" command from glibc.
-        wrapProgram $out/bin/initdb --prefix PATH ":" ${glibc.bin}/bin
-      '';
-
-    disallowedReferences = [ stdenv.cc ];
-
-    passthru = {
-      inherit readline psqlSchema;
-    };
+  LC_ALL = "C";
 
     # meta = with lib; {
     #   homepage = http://www.postgresql.org/;
@@ -86,15 +39,4 @@ let
     #   platforms = platforms.unix;
     #   hydraPlatforms = platforms.linux;
     # };
-  });
-
-in {
-
-  agensgraph = common {
-    version = "1.1.0";
-    pgVersion = "9.6.2";
-    psqlSchema = "9.6";
-    sha256 = "0qbmzmkl36wv5d66k7s0p0xll4ai1ax3pczrfgxbmfk9i2j1gjzw";
-  };
-
 }
