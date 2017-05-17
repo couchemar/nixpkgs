7e04aa4b48637d0ef17eb50a99d08902681c663b
Andrey Pavlov <couchemar@yandex.ru>
2017-05-17 08:34:47 +0300

    Собираем agensgraph из мастера

--- pkgs/servers/sql/agensgraph/default.nix
+++ pkgs/servers/sql/agensgraph/default.nix
@@ -1,14 +1,22 @@
-{ lib, stdenv, glibc, fetchurl, zlib, readline, libossp_uuid, openssl, makeWrapper, perl, bison, flex }:
+{ fetchFromGitHub, lib, stdenv, glibc, fetchurl, zlib, readline, libossp_uuid, openssl, makeWrapper, perl, bison, flex }:
 
  
 stdenv.mkDerivation rec {
   name = "agensgraph-${version}";
   version = "1.1.0";
 
-  src = fetchurl {
-    url = "https://github.com/bitnine-oss/agensgraph/archive/v${version}.tar.gz";
-    sha256 = "0qbmzmkl36wv5d66k7s0p0xll4ai1ax3pczrfgxbmfk9i2j1gjzw";
-  };
+  # src = fetchurl {
+  #   url = "https://github.com/bitnine-oss/agensgraph/archive/v${version}.tar.gz";
+  #   sha256 = "0qbmzmkl36wv5d66k7s0p0xll4ai1ax3pczrfgxbmfk9i2j1gjzw";
+  # };
+
+            src = fetchFromGitHub {
+              owner = "bitnine-oss";
+              repo = "agensgraph";
+              rev = "6a717c821beadafb8e821ed4e2c34be6f742aea0";
+              sha256 = "079zcscqmgx1jrvrcxb8i947k39dzd44m0cs4llqnwww2hsdlm5r";
+            };
+
 
   # outputs = [ "out" "lib" ];
   # setOutputFlags = false; # $out retains configureFlags :-/
